2b00e5abbd8c6639feabe501dde9dba2cb487365
Jacopo Essenziale <j.essenziale@gmail.com>
2025-04-18 21:10:07 +0200

    Change PageTracker to track hash routes

--- src/Components/PageTracker.js
+++ src/Components/PageTracker.js
@@ -5,9 +5,12 @@ const PageTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
+    const hashPath = location.hash || window.location.hash || "/";
+    const cleanedPath = hashPath.replace("#", "") || "/";
+
     if (window.gtag) {
       window.gtag("config", "G-640BWMC1HB", {
-        page_path: location.pathname,
+        page_path: cleanedPath,
       });
     }
   }, [location]);
